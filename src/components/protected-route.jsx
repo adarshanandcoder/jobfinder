5acb950f748927b150d9a0219b3095e3bda62e7a
Adarsh Anand <105978827+adarshanandcoder@users.noreply.github.com>
2025-01-15 18:38:13 +0530

    Update protected-route.jsx

--- src/components/protected-route.jsx
+++ src/components/protected-route.jsx
@@ -1,21 +1,26 @@
-import { useUser } from '@clerk/clerk-react'
-import React from 'react'
+import { useUser } from '@clerk/clerk-react';
+import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 
-const ProtectedRoute = ({children}) => {
-
-    const {isLoaded , user, isSignedIn} = useUser();
+const ProtectedRoute = ({ children }) => {
+    const { isLoaded, user, isSignedIn } = useUser();
     const { pathname } = useLocation();
 
-    if(isLoaded && !isSignedIn && isSignedIn !== undefined){
-        return <Navigate to='/?sign-in=true' />
+    if (!isLoaded) {
+        // You can add a loading spinner or similar UI here
+        return <div>Loading...</div>;
     }
 
-    if(user !== undefined && !user?.unsafeMetadata?.role && pathname !== "/onboarding"){
-      return <Navigate to="/onboarding" />
+    if (!isSignedIn) {
+        return <Navigate to='/?sign-in=true' />;
     }
 
-  return children;
-}
+    // If user is signed in but doesn't have a role and is not on the onboarding page
+    if (user && !user?.unsafeMetadata?.role && pathname !== '/onboarding') {
+        return <Navigate to="/onboarding" />;
+    }
 
-export default ProtectedRoute
+    return children;
+};
+
+export default ProtectedRoute;
